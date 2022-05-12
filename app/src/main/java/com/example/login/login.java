5f2cfbf975dca5c0dcc0c99438fa6633a77d9a6a
thato <thatosenoamadi007@gmail.com>
2022-05-12 18:51:14 +0200

    Its a commit by me

--- app/src/main/java/com/example/login/login.java
+++ app/src/main/java/com/example/login/login.java
@@ -1,5 +1,6 @@
 package com.example.login;
 // this is thapelo's first comment
+//this is Thato's first comment
 //ddddhhdjjdjd
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
