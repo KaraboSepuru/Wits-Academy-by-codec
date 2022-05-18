7bc8df2db2be0ec40c8db107382233d6b6df0f21
thato <thatosenoamadi007@gmail.com>
2022-05-18 17:02:34 +0200

    Some work on Quiz

--- app/src/main/java/com/example/login/login.java
+++ app/src/main/java/com/example/login/login.java
@@ -29,15 +29,15 @@ import com.google.firebase.database.ValueEventListener;
 import java.util.regex.Pattern;
 
 public class login extends AppCompatActivity {
+
     private EditText eLoginEmail;
     private EditText eLoginPassword;
     private Button btnLogin;
     private TextView tvRegisterHere;
-
     private FirebaseAuth mAuth;
-
     private RadioGroup radioGroup;
     private RadioButton radioButton;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
--- app/src/main/java/com/example/login/register.java
+++ app/src/main/java/com/example/login/register.java
@@ -1,6 +1,6 @@
 package com.example.login;
-//      Thato adds a comment
-        import androidx.annotation.NonNull;
+
+import androidx.annotation.NonNull;
         import androidx.appcompat.app.AppCompatActivity;
         import androidx.core.content.ContextCompat;
 
@@ -23,87 +23,92 @@ package com.example.login;
         import com.google.firebase.database.FirebaseDatabase;
 
 public class register extends AppCompatActivity {
-    EditText eRegEmail;
-    EditText eRegPassword;
-    TextView tvLoginHere;
 
-    Button btnRegister;
+    //Declare all views including Firebase
 
-    RadioGroup radioGroup;
-    RadioButton radioButton;
+    private EditText eRegEmail;
+    private EditText eRegPassword;
+    private Button btnRegister;
+    private TextView tvLoginHere;
+    private RadioGroup radioGroup;
+    private RadioButton radioButton;
+    private FirebaseAuth mAuth;
 
-    FirebaseAuth mAuth;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
 
-        getWindow().setStatusBarColor(ContextCompat.getColor(register.this, R.color.teal_700));
+        getWindow().setStatusBarColor(ContextCompat.getColor(register.this, R.color.teal_700));  //set status bar color
 
+        //intilise all view and get instance of Firebase
         radioGroup = findViewById(R.id.radioGroup);
-
-
         eRegEmail=findViewById(R.id.etRegEmail);
         eRegPassword=findViewById(R.id.etRegPass);
         tvLoginHere=findViewById(R.id.tvLoginHere);
         btnRegister=findViewById(R.id.btnRegister);
-
         mAuth=FirebaseAuth.getInstance();
 
+        //Implement the onClickListerner for the register button
         btnRegister.setOnClickListener(view ->{
-            createUser();
+            createUser();                                                                               //call function on Register button click
         });
 
+        //Implement the onClickListerner for the re
         tvLoginHere.setOnClickListener(view ->{
             startActivity(new Intent(register.this,login.class));
         });
     }
 
+    /*Create user function*/
     private void createUser() {
 
+        //Get checked occupation, teacher or student to create account for
         int radioID = radioGroup.getCheckedRadioButtonId();
         radioButton = findViewById(radioID);
-
         String occupation = radioButton.getText().toString();
 
+        //Get email and password from user
         String email = eRegEmail.getText().toString().trim();
         String password = eRegPassword.getText().toString().trim();
 
+
         if(TextUtils.isEmpty(email)){
             eRegEmail.setError("Email cannot be empty");
-            eRegEmail.requestFocus();
+            eRegEmail.requestFocus();                                                               //If email is empty notify user and request input by requesting focus
         }else if(TextUtils.isEmpty(password)){
             eRegPassword.setError("Password cannot be empty");
-            eRegPassword.requestFocus();
+            eRegPassword.requestFocus();                                                            //Do the same as above for password
         }else{
-            mAuth.createUserWithEmailAndPassword(email,password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+            mAuth.createUserWithEmailAndPassword(email,password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {       //If they not empty create user with email and passoword which is not stored on database
                 @Override
                 public void onComplete(@NonNull Task<AuthResult> task) {
                     if(task.isSuccessful()){
-                        User user = new User(occupation,password,email);
+                        User user = new User(password,email);                                                                       //Instantiate a new user object
 
-                        FirebaseDatabase.getInstance().getReference("Users")
-                                .child(FirebaseAuth.getInstance().getCurrentUser().getUid())
-                                .setValue(user).addOnCompleteListener(new OnCompleteListener<Void>() {
+                        FirebaseDatabase.getInstance().getReference(occupation)
+                                .child(FirebaseAuth.getInstance().getCurrentUser().getUid())                                        //Add user to table in Firebase based on their occupation including their ID
+                                .setValue(user).addOnCompleteListener(new OnCompleteListener<Void>() {                                 //Can be on Success Listener
                             @Override
                             public void onComplete(@NonNull Task<Void> task) {
                                 if(task.isSuccessful()){
                                     if(occupation.equals("Teacher")){
-                                        Toast.makeText(register.this,"Teacher's account registered successfully",Toast.LENGTH_SHORT).show();
+                                        Toast.makeText(register.this,"Teacher's account registered successfully",Toast.LENGTH_SHORT).show();            //If user added successfully then make toast for Teacher or student
                                     }else{
                                         Toast.makeText(register.this,"Student's account registered successfully",Toast.LENGTH_SHORT).show();
                                     }
 
-                                    startActivity(new Intent(register.this,login.class));
+                                    startActivity(new Intent(register.this,login.class));                               //Move user to log in page
                                 }else{
-                                    Toast.makeText(register.this,"Registration Error: "+task.getException().getMessage(),Toast.LENGTH_SHORT).show();
+                                    Toast.makeText(register.this,"Registration Error: "+task.getException().getMessage(),Toast.LENGTH_SHORT).show();        //If unsuccessful registering
                                 }
                             }
                         });
 
 
                     }else{
-                        Toast.makeText(register.this,"Registration Error: "+task.getException().getMessage(),Toast.LENGTH_SHORT).show();
+                        Toast.makeText(register.this,"Registration Error: "+task.getException().getMessage(),Toast.LENGTH_SHORT).show();                //Could'nt create user
 
                     }
                 }
