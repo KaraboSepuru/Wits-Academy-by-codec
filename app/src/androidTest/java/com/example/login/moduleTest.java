47d0ee8faf2d68bb2e9073822163582f60af0d91
ThapeloSeletisha <thapeloseletisha01@gmail.com>
2022-05-23 15:17:25 +0200

    added tests

---
+++ app/src/androidTest/java/com/example/login/moduleTest.java
@@ -0,0 +1,22 @@
+package com.example.login;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class moduleTest {
+
+    public module testing = new module("STAT2006","Mathematical Statistics 2","Mr Sergio Giavani", 2.0f);
+
+    @Test
+    public void testIsModCodCorrect(){
+        Assert.assertEquals(testing.getModCode(),"STAT2006");
+    }
+    @Test
+    public void testIsModNameCorrect(){
+        Assert.assertEquals(testing.getModName(),"Mathematical Statistics 2");
+    }
+    @Test
+    public void testIsModTeacherCorrect(){
+        Assert.assertEquals(testing.getModTeacher(), "Mr Sergio Giavani");
+    }
+}
