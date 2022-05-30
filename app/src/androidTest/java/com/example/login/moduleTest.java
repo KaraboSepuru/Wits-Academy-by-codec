215b45900da7f75071d21a13f89cf28be308d53a
ThapeloSeletisha <thapeloseletisha01@gmail.com>
2022-05-30 15:59:12 +0200

    added module test

--- app/src/androidTest/java/com/example/login/moduleTest.java
+++ app/src/androidTest/java/com/example/login/moduleTest.java
@@ -1,8 +1,12 @@
 package com.example.login;
 
+import androidx.test.ext.junit.runners.AndroidJUnit4;
+
 import org.junit.Assert;
 import org.junit.Test;
+import org.junit.runner.RunWith;
 
+@RunWith(AndroidJUnit4.class)
 public class moduleTest {
 
     public module testing = new module("STAT2006","Mathematical Statistics 2","Mr Sergio Giavani", 2.0f);
