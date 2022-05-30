58c2d1312757d2fb365d85bd15cf7fa2fc92e689
ThapeloSeletisha <thapeloseletisha01@gmail.com>
2022-05-30 16:50:16 +0200

    fixed module class bug

--- app/src/test/java/com/example/login/ExampleUnitTest.java
+++ app/src/test/java/com/example/login/ExampleUnitTest.java
@@ -18,11 +18,11 @@ public class ExampleUnitTest {
     public module testing = new module("STAT2006","Mathematical Statistics 2","Mr Sergio Giavani", 2.0f);
     @Test
     public void testIsModCodCorrect(){
-        Assert.assertEquals(testing.getModName(),"STAT2006");
+        Assert.assertEquals(testing.getModCode(),"STAT2006");
     }
     @Test
     public void testIsModNameCorrect(){
-        Assert.assertEquals(testing.getModCode(),"Mathematical Statistics 2");
+        Assert.assertEquals(testing.getModName(),"Mathematical Statistics 2");
     }
     @Test
     public void testIsModTeacherCorrect(){
